1d4bb0d1b98bef9e84d7e43ea7927685a300f84a
CJ <cjzhang88@gmail.com>
2017-08-26 12:16:03 +0800

    价格帅选

--- server/routes/goods.js
+++ server/routes/goods.js
@@ -5,36 +5,73 @@ var Goods = require('../models/goods');
 
 mongoose.connect('mongodb://127.0.0.1:27017/mall');
 
-mongoose.connection.on("connected",()=>{
-  console.log("MonogoDB connected success");
+mongoose.connection.on("connected", () => {
+    console.log("MonogoDB connected success");
 });
 
-mongoose.connection.on("error",()=>{
-  console.log("MonogoDB connected fail");
+mongoose.connection.on("error", () => {
+    console.log("MonogoDB connected fail");
 });
-mongoose.connection.on("disconected",()=>{
-  console.log("MonogoDB disconnected")
+mongoose.connection.on("disconected", () => {
+    console.log("MonogoDB disconnected")
 });
 
 
-router.get("/", (req, res,next) => {
-  Goods.find({}, (error, doc)=> {
-     if(error){
-       res.json({
-         status: '1',
-         msg: error.message
-       });
-     } else {
-       res.json({
-         status:'0',
-         msg:'',
-         result: {
-          count: doc.length,
-          list: doc
-         } 
-       })
-     }
-  })
+router.get("/", (req, res, next) => {
+    let page = parseInt(req.param("page"));
+    let pageSize = parseInt(req.param("pageSize"));
+    let priceLevel = req.param("priceLevel");
+    let sort = req.param("sort");
+    let skip = (page - 1) * pageSize;
+    let params = {};
+    let priceGt = '',
+        priceLte = '';
+    if (priceLevel != 'all') {
+        switch (priceLevel) {
+            case '0':
+                priceGt = 0;
+                priceLte = 100;
+                break;
+            case '1':
+                priceGt = 100;
+                priceLte = 500;
+                break;
+            case '2':
+                priceGt = 500;
+                priceLte = 1000;
+                break;
+            case '3':
+                priceGt = 1000;
+                priceLte = 5000;
+                break;
+        }
+        params = {
+            salePrice: {
+                $gt: priceGt,
+                $lte: priceLte
+            }
+        }
+    }
+
+    let goodsModel = Goods.find(params).skip(skip).limit(pageSize);
+    goodsModel.sort({ 'salePrice': sort });
+    goodsModel.exec((error, doc) => {
+        if (error) {
+            res.json({
+                status: '1',
+                msg: error.message
+            });
+        } else {
+            res.json({
+                status: '0',
+                msg: '',
+                result: {
+                    count: doc.length,
+                    list: doc
+                }
+            })
+        }
+    })
 });
 
 
